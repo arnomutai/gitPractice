ad3b2488587a91332fbd459f6e1620b713664a0f
arnomutai <arnomutai@gmail.com>
2021-08-17 13:29:22 +0300

    cooms

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get "about", to: "about#index"
+  get "sign_up", to: "registration#new"
+  post "sign_up", to: "registration#create"
   root to: "main#index"
 end
